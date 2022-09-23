dfd2084f1ca844f21359acba3b1bbe6fc2a7e316
Tsar Bomba Nick <njkuechel@protonmail.com>
2022-09-23 03:01:13 -0600

    Created --checkcal flag

--- autonicer/__init__.py
+++ autonicer/__init__.py
@@ -14,8 +14,19 @@ def run():
         help="Set the src from the command line",
         type=str,
     )
+    p.add_argument(
+        "-checkcal",
+        "--checkcal",
+        help = "Checks if mpu7_cl.evt are up to date with latest NICER calibrations",
+        action="store_true",
+        default = False,
+    )
 
     args = p.parse_args()
-    an = autonicer.AutoNICER(args.src)
-    an.call_nicer()
-    an.command_center()
+    if args.checkcal == True:
+        print("Checkcal called")
+    
+    else:
+        an = autonicer.AutoNICER(args.src)
+        an.call_nicer()
+        an.command_center()
