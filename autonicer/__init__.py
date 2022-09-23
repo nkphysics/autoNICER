2e1900b3e31567bdac8aefd165c48cdce69f326f
Tsar Bomba Nick <njkuechel@protonmail.com>
2022-09-23 02:38:13 -0600

    Migration from click to argparse

--- autonicer/__init__.py
+++ autonicer/__init__.py
@@ -1,18 +1,21 @@
 from . import autonicer
 from .autonicer import AutoNICER
-import click
+import argparse as ap
 
-@click.command(context_settings={"help_option_names": ["-h", "--help"]},
-				help="A program for piplining NICER data reduction. Run by just typing autonicer.")
-@click.option("--src", 
-				type = str, 
-				default = None,
-				help = "Option to set the src from the command line")
-#@click.option("--reprocess",
-#				is_flag = True,
-#				help = "Navigate to OBSID directory and issue --reprocess to reprocess the OBSID dataset with new commands/calibrations")
 
-def run(src):
-	an = autonicer.AutoNICER(src)
-	an.call_nicer()
-	an.command_center()
+def run():
+    p = ap.ArgumentParser(
+        description="A program for piplining NICER data reduction. Run by just typing autonicer."
+    )
+
+    p.add_argument(
+        "-src",
+        "--src",
+        help="Set the src from the command line",
+        type=str,
+    )
+
+    args = p.parse_args()
+    an = autonicer.AutoNICER(args.src)
+    an.call_nicer()
+    an.command_center()
