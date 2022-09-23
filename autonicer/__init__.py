cc4805b5268044a2e7be88ccae2ac50a90c019aa
Tsar Bomba Nick <njkuechel@protonmail.com>
2022-09-23 05:59:24 -0600

    Added --force flag to the cli

--- autonicer/__init__.py
+++ autonicer/__init__.py
@@ -14,18 +14,27 @@ def run():
         help="Set the src from the command line",
         type=str,
     )
+
     p.add_argument(
         "-checkcal",
         "--checkcal",
-        help = "Checks if mpu7_cl.evt are up to date with latest NICER calibrations",
+        help="Checks if mpu7_cl.evt are up to date with latest NICER calibrations",
         action="store_true",
-        default = False,
+        default=False,
+    )
+
+    p.add_argument(
+        "-force",
+        "--force",
+        help="Forces a reprocess if calibrations are not up to date with latest CALDB",
+        action="store_true",
+        default=False,
     )
 
     args = p.parse_args()
     if args.checkcal == True:
         checkcal()
-    
+
     else:
         an = autonicer.AutoNICER(args.src)
         an.call_nicer()
