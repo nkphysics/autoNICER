26703b782c9dc44830ecc75e80950c9e0215ce4a
Tsar Bomba Nick <njkuechel@protonmail.com>
2022-10-22 19:35:37 -0600

    Set calibration status variable ownership to Reprocess class and formatted status message

--- autonicer/reprocess.py
+++ autonicer/reprocess.py
@@ -2,29 +2,40 @@ from .autonicer import get_caldb_ver
 import subprocess as sp
 import os
 from astropy.io import fits
+from termcolor import colored
 
 
 class Reprocess:
     def __init__(self):
         self.curr_caldb = get_caldb_ver()
+        self.last_caldb = None
+        self.calstate = None
 
     def checkcal(self):
         """
         Checks the status of an existing obsid's calibrations
         """
-        calstate = False
         print(f"Latest NICER CALDB: {self.curr_caldb}")
         base_dir = os.getcwd()
         os.chdir(f"xti/event_cl/")
         files = sp.run("ls *cl.evt", shell=True, capture_output=True, encoding="utf-8")
-        last_caldb = 0
         for i in str(files.stdout).split("\n"):
             if i == "":
                 break
             print(f"Detected mpu7_cl.evt files: {i}")
             hdul = fits.open(i)
-            last_caldb = hdul[0].header["CALDBVER"]
-            print(f"CALDB for {i}: {last_caldb}")
-            if last_caldb == self.curr_caldb:
-                calstate = True
-        return calstate
+            self.last_caldb = hdul[0].header["CALDBVER"]
+            print(f"CALDB for {i}: {self.last_caldb}")
+            
+            premessage = ""
+            color = "red"
+            if self.last_caldb == self.curr_caldb:
+                self.calstate = True
+                color = "green"
+            else:
+                self.calstate = False
+                premessage = "NOT"
+            print(colored(f"{premessage} Up to date with latest NICER CALDB", color))
+            hdul.close()
+        return self.calstate       
+        
