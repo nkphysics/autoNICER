4e850b9256a6e9cc3f7930ee39b75f178729d365
Tsar Bomba Nick <njkuechel@protonmail.com>
2022-10-22 20:16:45 -0600

    Created Reprocess.get_evts()

--- autonicer/reprocess.py
+++ autonicer/reprocess.py
@@ -3,6 +3,7 @@ import subprocess as sp
 import os
 from astropy.io import fits
 from termcolor import colored
+from .autonicer import AutoNICER
 
 
 class Reprocess:
@@ -10,6 +11,19 @@ class Reprocess:
         self.curr_caldb = get_caldb_ver()
         self.last_caldb = None
         self.calstate = None
+        self.src = None
+        self.clevts = self.get_clevts()
+        
+    def get_clevts(self):
+        base_dir = os.getcwd()
+        os.chdir(f"xti/event_cl/")
+        files = sp.run("ls *cl.evt", shell=True, capture_output=True, encoding="utf-8")
+        filelist = []
+        for i in str(files.stdout).split("\n"):
+            if i != "":
+                filelist.append(i)
+        os.chdir(base_dir)
+        return filelist
 
     def checkcal(self):
         """
@@ -36,6 +50,13 @@ class Reprocess:
                 self.calstate = False
                 premessage = "NOT"
             print(colored(f"{premessage} Up to date with latest NICER CALDB", color))
+            print("")
             hdul.close()
-        return self.calstate       
+        return self.calstate
+       
+    def reprocess(self):
+        """
+        Reprocesses an existing dataset with latest calibrations
+        """
+        an = autonicer.AutoNICER()
         
